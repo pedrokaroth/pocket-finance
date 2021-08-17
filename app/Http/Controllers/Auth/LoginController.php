f08f67c3ee7e342c20fcd8b0e06148e54516fc25
pedrokaroth <pedrokaroth@gmail.com>
2021-08-16 23:34:08 -0300

    layout master da app adicionado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,22 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(Auth::attempt($credentials)) {
+            return redirect()->route('app.home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email ou senha inválido'
+        ]);
+
     }
 }
---
+++ resources/views/app/includes/header.blade.php
@@ -0,0 +1,8 @@
+<header class="app-header">
+    <h1>
+        <a href="{{ route('app.home') }}" class="">
+            <i class="fas fa-money-bill-wave"></i>
+            PocketApp
+        </a>
+    </h1>
+</header>
