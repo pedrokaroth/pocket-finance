b9e2dd5e83bc2892c6bc04567458e74440229315
pedrokaroth <pedrokaroth@gmail.com>
2021-08-27 10:59:29 -0300

    Alterações estilo navbar active

--- resources/views/app/includes/nav.blade.php
+++ resources/views/app/includes/nav.blade.php
@@ -7,12 +7,12 @@
     </div>
     <hr>
     <div class="sidebar-nav">
-        <a href="{{ route('app.wallets') }}" class="nav_link radius {{ isActive('app.wallets') }}">
+        <a href="{{ route('app.wallets') }}" class="nav_link {{ isActive('app.wallets') }}">
             <i class="fas fa-wallet"></i>
             <span>Carteiras</span>
         </a>
         <div class="logout">
-            <a href="{{ route('logout') }}" class="nav_link radius" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+            <a href="{{ route('logout') }}" class="nav_link" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                 <i class="fas fa-sign-out-alt"></i>
                 <span style="">Sair</span>
             </a>
