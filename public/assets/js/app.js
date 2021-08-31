d81aadb1a6dc7cb2fd10e2cb53281b0832417dd1
pedrokaroth <pedrokaroth@gmail.com>
2021-08-31 15:41:28 -0300

    ajustes tamanho filtro

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,97 @@
-$((function(){const e=$(".app");if(toastr.options={closeButton:!1,debug:!1,newestOnTop:!0,progressBar:!0,positionClass:"toast-top-right",preventDuplicates:!1,onclick:null,showDuration:"300",hideDuration:"1000",timeOut:"5000",extendedTimeOut:"1000",showEasing:"swing",hideEasing:"linear",showMethod:"fadeIn",hideMethod:"fadeOut"},e.data().hasOwnProperty("message")){let t=e.data("type"),a=e.data("message");switch(t){case"success":toastr.success(a)}}$.ajaxSetup({headers:{"X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")}}),$("form:not('.ajax-off')").submit((function(e){e.preventDefault();const t=$(this),a=t.serialize(),o=t.attr("action");$.ajax({url:o,type:"POST",data:a,dataType:"json",success:function(e){e.reload&&window.location.reload()},error:function(e){const t=e.responseJSON.errors;Object.keys(t).forEach((e=>{toastr.error(t[e])}))}})})),$(".open-modal").click((function(){if($(".app-modal").length){let e=$(this).data("modal");$("#"+e).modal("show")}})),$(".mask-money").mask("000.000.000.000.000,00",{reverse:!0,placeholder:"0,00"}),$(".select2").select2({width:"120px",height:"100%"})}));
+$(function() {
+    const app = $('.app');
+
+    /*
+    *   TOASTR
+    */
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    /*
+    *   FLASH
+    */
+
+    if (app.data().hasOwnProperty('message')) {
+        let type = app.data('type');
+        let message = app.data('message');
+
+        switch (type){
+            case 'success':
+                toastr.success(message);
+                break;
+        }
+    }
+
+    /*
+    *   AJAX
+    */
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    })
+
+    $("form:not('.ajax-off')").submit(function(event) {
+        event.preventDefault();
+        const form = $(this);
+        const data = form.serialize();
+        const action = form.attr('action');
+
+        $.ajax({
+            url: action,
+            type: 'POST',
+            data: data,
+            dataType: 'json',
+
+            success: function(response) {
+                if(response.reload) {
+                    window.location.reload();
+                }
+            },
+
+            error: function(response) {
+                const errors = response.responseJSON.errors;
+
+                Object.keys(errors).forEach(key => {
+                    toastr.error(errors[key]);
+                });
+            }
+        })
+    })
+
+    $('.open-modal').click(function() {
+        if ($(".app-modal").length) {
+            let modal = $(this).data('modal');
+            $('#' + modal).modal('show');
+        }
+    })
+    /*
+    *   MASK
+    */
+    $(".mask-money").mask('000.000.000.000.000,00', {
+        reverse: true,
+        placeholder: "0,00"
+    });
+    /*
+    *   SELECT2
+    */
+    $('.select2').select2({
+        width: '150',
+        height: '100%'
+    });
+})
