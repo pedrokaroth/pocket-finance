acb84629b5b36461f37747ed9950880e4487ad1a
Pedro Dimbarre <pedrokaroth@gmail.com>
2021-09-06 15:05:40 -0300

    Update Helpers.php

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -47,8 +47,6 @@ if (! function_exists('filterValidate')) {
 
                     return (is_numeric($m) && 0 < $m && $m < 13) && (is_numeric($y));
                 }
-            default:
-                return false;
         }
         
         return false;
