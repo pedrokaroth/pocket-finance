a47fca3781474589d2143e746ef26237ed20fd85
pedrokaroth <pedrokaroth@gmail.com>
2021-09-06 21:39:50 -0300

    validação para usuários ainda não ativos que não possuem carteira criada

--- resources/views/app/includes/header.blade.php
+++ resources/views/app/includes/header.blade.php
@@ -5,14 +5,16 @@
             PocketApp
         </a>
     </h1>
-    <ul class="header-wallet">
-        <li class="wallet-dropdown">
-            {{ walletactive()->wallet }}
-            <ul style="display: none">
-                @foreach(wallets() as $wallet)
-                    <li data-wallet="{{ $wallet->id }}" data-endpoint="{{ route('app.wallets.filter', $wallet->id) }}" class="wallet-change">{{ $wallet->wallet }}</li>
-                @endforeach
-            </ul>
-        </li>
-    </ul>
+    @if(\App\Models\App\Wallet::free())
+        <ul class="header-wallet">
+            <li class="wallet-dropdown">
+                    {{ walletactive()->wallet }}
+                    <ul style="display: none">
+                        @foreach(wallets() as $wallet)
+                            <li data-wallet="{{ $wallet->id }}" data-endpoint="{{ route('app.wallets.filter', $wallet->id) }}" class="wallet-change">{{ $wallet->wallet }}</li>
+                        @endforeach
+                    </ul>
+            </li>
+        </ul>
+    @endif
 </header>
