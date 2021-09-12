64dc4fa96471f76888bbf6f3a6754ae1184139d1
pedrokaroth <pedrokaroth@gmail.com>
2021-09-12 12:40:57 -0300

    tradução de dias da semana

--- resources/lang/pt_BR/messages.php
+++ resources/lang/pt_BR/messages.php
@@ -3,5 +3,15 @@
 return [
     'weekly' => 'semanalmente',
     'monthly' => 'mensalmente',
-    'annually' => 'anualmente'
+    'annually' => 'anualmente',
+    /*
+     * DAYS
+     */
+    'Sun' => 'domingo',
+    'Mon' => 'segunda-feira',
+    'Tue' => 'terça-feira',
+    'Wed' => 'quarta-feira',
+    'Thu' => 'quinta-feira',
+    'Fri' => 'sexta-feira',
+    'Sat' => 'sabado'
 ];
